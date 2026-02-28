c010b365af35c42647f9238c7e4ef6889b05a25e
strusxsure <tufailriyyan7@gmail.com>
2026-02-28 11:36:58 +0500

    Initial plugin generation

---
+++ src/main/java/com/stormai/plugin/Main.java
@@ -0,0 +1,20 @@
+package com.stormai.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+
+public class Main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
+    }
+}
